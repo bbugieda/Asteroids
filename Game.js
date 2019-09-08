7d4ac69453c1e1b803db5b2d54681de6a177eab3
Korey Mitchell <kmitchel@udel.edu>
2019-09-08 18:59:12 -0400

    Smoother multi-directional movement

--- Game.js
+++ Game.js
@@ -19,16 +19,20 @@ var change = {
       transform: "rotate(180deg)"
     },
   }
-  $(document).one("keydown", keyDown)
+  $(document).on({
+    keydown: keydown,
+    keyup: keyup
+  })
   
-  var going;
+  var movement = []
   
-  function keyDown(e) {
-    console.log("down")
-    $(document).one("keyup", keyup)
-    var animation = change[e.which];
-    going = setInterval(keepGoing, 1);
-  
+  function keydown(e) {
+    var key = e.which
+    var animation = change[key];
+    if (!movement[key]) { // watch out for repeating keys!
+      movement[key] = setInterval(keepGoing, 1)
+    }
+    //  console.log("down", key, movement[key])
     function keepGoing() {
       $(".ball").css(animation)
     }
@@ -36,77 +40,7 @@ var change = {
   }
   
   function keyup(e) {
-    console.log("up")
-    clearInterval(going)
-    $(document).one("keydown", keyDown)
-  }
-  var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext("2d");
-
-canvas.width = canvas.height = 300;
-
-var x = 150,
-    y = 150,
-    velY = 0,
-    velX = 0,
-    speed = 2,
-    friction = 0.98,
-    keys = [];
-
-function update() {
-    requestAnimationFrame(update);
-    
-    if (keys[38]) {
-        if (velY > -speed) {
-            velY--;
-        }
-    }
-    
-    if (keys[40]) {
-        if (velY < speed) {
-            velY++;
-        }
-    }
-    if (keys[39]) {
-        if (velX < speed) {
-            velX++;
-        }
-    }
-    if (keys[37]) {
-        if (velX > -speed) {
-            velX--;
-        }
-    }
-
-    velY *= friction;
-    y += velY;
-    velX *= friction;
-    x += velX;
-
-    if (x >= 295) {
-        x = 295;
-    } else if (x <= 5) {
-        x = 5;
-    }
-
-    if (y > 295) {
-        y = 295;
-    } else if (y <= 5) {
-        y = 5;
-    }
-
-    ctx.clearRect(0, 0, 300, 300);
-    ctx.beginPath();
-    ctx.
-    ctx.arc(x, y, 5, 0, Math.PI * 2);
-    ctx.fill();
-}
-
-update();
-
-document.body.addEventListener("keydown", function (e) {
-    keys[e.keyCode] = true;
-});
-document.body.addEventListener("keyup", function (e) {
-    keys[e.keyCode] = false;
-});
+    var key = e.which
+    movement[key] = clearInterval(movement[key])
+      //  console.log("up", key, movement[key])
+  }  
