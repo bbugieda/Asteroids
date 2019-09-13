62afb460ff6d9e8a41a56576d9c3faa8d4a248fc
Korey Mitchell <kmitchel@udel.edu>
2019-09-13 18:48:46 -0400

    Updates to rocket movement

--- Game.js
+++ Game.js
@@ -1,49 +1,52 @@
-var change = {
-    37: {
-      left: "-=1",
-      transform: "rotate(270deg)"
-    },
-  
-    38: {
-      top: "-=1",
-      transform: "rotate(0deg)"
-    },
-  
-    39: {
-      left: "+=1",
-      transform: "rotate(90deg)"
-    },
-  
-    40: {
-      top: "+=1",
-      transform: "rotate(180deg)"
-    },
-  }
-  $(document).on({
-    keydown: keydown,
-    keyup: keyup
-  })
-  
-  var movement = []
-  
-  function keydown(e) {
-    var key = e.which
-    var animation = change[key];
-    if (!movement[key]) { // watch out for repeating keys!
-      movement[key] = setInterval(keepGoing, 1)
+$(document).ready(function(e){
+  var keys = {};
+
+  $(document).keydown(function(event){
+    keys[event.which] = true;
+  }).keyup(function(event){
+    delete keys[event.which];
+  });
+
+  var $d = $(".ball");
+  var angle = 0
+
+  function gameLoop() {
+    if (angle >= 360){
+      angle = 0
     }
-    //  console.log("down", key, movement[key])
-    function keepGoing() {
-      for (i = 0; i < 1; i++) {
-        $(".ball").css(animation)
-      }
-      
+    else if (angle <= 0){
+      angle = 359
     }
-  
+
+    if (keys[39]) {     //right
+        angle+=5
+        $d.css("transform", "rotate("+angle+"deg)")
+    }
+    else if (keys[37]) { //left
+        angle-=5
+        $d.css("transform", "rotate("+angle+"deg)")
+    }
+    if (keys[40]) { //down
+        var xloc = parseFloat($d.css('left'));
+        var yloc = parseFloat($d.css('top'));
+        xloc += Math.cos(angle)
+        yloc += Math.sin(angle)
+        $d.css("top", yloc + 'px');
+        $d.css("left", xloc + 'px');
+    }
+    else if (keys[38]) { //up
+      var xloc = parseFloat($d.css('left'));
+      var yloc = parseFloat($d.css('top'));
+      xloc += 4*Math.cos(4.8+ angle * Math.PI/180)
+      yloc += 4*Math.sin(4.8+ angle * Math.PI/180)
+      $d.css("top", yloc + "px");
+      $d.css("left", xloc + "px");
+    }
+ 
+
+    setTimeout(gameLoop, 20);
   }
-  
-  function keyup(e) {
-    var key = e.which
-    movement[key] = clearInterval(movement[key])
-      //  console.log("up", key, movement[key])
-  }  
+  gameLoop();
+    
+  $(document).focus();
+});
