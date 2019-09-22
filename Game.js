7acbc648ad9150c573b5c2619675dce0d77c3b82
MaxLuu29 <datluu829@gmail.com>
2019-09-22 13:30:36 -0400

    set up enums for directions

--- Game.js
+++ Game.js
@@ -1,3 +1,12 @@
+/**
+ * Enums for directions
+ */
+const direction = {
+	LEFT: 37,
+	RIGHT: 39,
+	UP: 38,
+	SPACE: 32,
+}
 
 $(document).ready(function() {
 	$("#StartButton").click(function () {
@@ -5,37 +14,36 @@ $(document).ready(function() {
 		$(".spaceShip").show();
 		$("body").show();
 	});
-	var keys = {};
+
+	let keys = {};
 	$(document)
 		.keydown(function (event) {
-			if (event.which == 39 || event.which == 37 || event.which == 32 || event.which == 38) {
+			if (event.which == direction.RIGHT || event.which == direction.LEFT || event.which == direction.SPACE || event.which == direction.UP) {
 				keys[event.which] = true;
 			}
 		})
 		.keyup(function (event) {
-			if (event.which == 39 || event.which == 37 || event.which == 32 || event.which == 38) {
+			if (event.which == direction.RIGHT || event.which == direction.LEFT || event.which == direction.SPACE || event.which == direction.UP) {
 				keys[event.which] = false;
 			}
 		});
 
-	var $ship = $(".spaceShip");
-	var angle = 0;
-	var accelerationX = 0;
-	var accelerationY = 0;
-	var velocityX = 0;
-	var velocityY = 0;
+	let $ship = $(".spaceShip");
+	let angle = 0;
+	let accelerationX = 0;
+	let accelerationY = 0;
+	let velocityX = 0;
+	let velocityY = 0;
 
 	$(window).height();
 	$(window).width();
-	var checkTime = 0;
-	var bulletCount = 0;
-	var currentTime = new Date();
+	let bulletCount = 0;
 
 	function areaCheck() {
-		var screeny = $(window).height();
-		var screenx = $(window).width();
-		var xloc = parseFloat($ship.css("left"));
-		var yloc = parseFloat($ship.css("top"));
+		let screeny = $(window).height();
+		let screenx = $(window).width();
+		let xloc = parseFloat($ship.css("left"));
+		let yloc = parseFloat($ship.css("top"));
 
 		if (xloc > screenx) {
 			$ship.css("left", 0 + "px");
@@ -53,13 +61,13 @@ $(document).ready(function() {
 
 	function fireBullet(x, y, ang) {
 		bulletCount++;
-		var t = $("body").append(
+		let t = $("body").append(
 			$("<img src='assets/bullet.png'>")
 				.addClass("bullet")
 				.css({ left: x + 60, top: y + 20 })
 		);
 		t.attr("id", bulletCount);
-		var bull = new Bullet(x, y, ang);
+		let bull = new Bullet(x, y, ang);
 		return bull;
 	}
 
@@ -82,11 +90,11 @@ $(document).ready(function() {
 		});
 	}
 
-	var locked = false;
+	let locked = false;
 
 	function updatePosition() {
-		var xloc = parseFloat($ship.css("left"));
-		var yloc = parseFloat($ship.css("top"));
+		let xloc = parseFloat($ship.css("left"));
+		let yloc = parseFloat($ship.css("top"));
 		
 		// maximum x velocity
 		if (velocityX >= 7) {
@@ -110,28 +118,28 @@ $(document).ready(function() {
 
 	function gameLoop() {
 		areaCheck();
-		if (keys[39]) {
+		if (keys[direction.RIGHT]) {
 			//right
 			angle = (angle + 5) % 360;
 			$ship.css("transform", "rotate(" + angle + "deg)");
 		}
-		if (keys[37]) {
+		if (keys[direction.LEFT]) {
 			//left
 			angle = (angle - 5) % 360;
 			$ship.css("transform", "rotate(" + angle + "deg)");
 		}
-		if (keys[32]) {
+		if (keys[direction.SPACE]) {
 			//space
 			if (!locked) {
 				locked = true;
 
-				var xloc = parseFloat($ship.css("left"));
-				var yloc = parseFloat($ship.css("top"));
+				let xloc = parseFloat($ship.css("left"));
+				let yloc = parseFloat($ship.css("top"));
 
-				var bull = fireBullet(xloc, yloc, angle);
+				let bull = fireBullet(xloc, yloc, angle);
 				update(bull);
 
-				// var i = setInterval(update(bull), 50);
+				// let i = setInterval(update(bull), 50);
 				// setTimeout(function () {
 				// 	clearInterval(i);
 				// }, 5000);
@@ -140,12 +148,12 @@ $(document).ready(function() {
 				}, 250);
 			}
 		}
-		if (keys[38]) {
+		if (keys[direction.UP]) {
 			//up
 			accelerationX = 0.03 * Math.cos(4.8+(angle * Math.PI) / 180);
 			accelerationY = 0.03 * Math.sin(4.8+(angle * Math.PI) / 180);
 		}
-		if (!keys[38]) {
+		if (!keys[direction.UP]) {
 			// deceleration
 			velocityX *= 0.98;
 			velocityY *= 0.98;
